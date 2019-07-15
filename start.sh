467a125fd687d5b75a57d294112cba6c9a3299d6
alexvance89 <alexvance89@gmail.com>
2019-07-15 16:57:26 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 megadl --path=mc.zip ${MEGA_URL}
 unzip mc.zip -d /data-temp
-mv -v data-temp/*/* data/ && rm -r -f /data-temp && rm mc.zip
+#mv -v data-temp/*/* data/
+mv -v data-temp/*/!(server.properties|ops.json|banned-ips.json|banned-players.json|whitelist.json|usercache.json) data/
+rm -r -f /data-temp 
+rm -r -f ./mc.zip
 jarfile=$(cd /data && ls|grep forge-*.jar)
 cd /data && java -server "-Xmx${INIT_MEM}" "-Xms${MAX_MEM}" -jar $jarfile nogui
