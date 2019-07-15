6d164106d8529b28f784592e146140bbbbaab32b
alexvance89 <alexvance89@gmail.com>
2019-07-15 15:41:04 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-megadl --path=mc.zip ${MEGA_URL}
+megadl --path=mc.zip '${MEGA_URL}'
 unzip mc.zip -d /data-temp
 mv -v data-temp/*/* data/ && rm -r -f /data-temp && rm mc.zip
 jarfile=$(cd /data && ls|grep forge-*.jar)
